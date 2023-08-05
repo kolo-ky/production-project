86fbca5525eef96685ca20d6a1ba0bf7b809c894
Alexander <kolo-ky@yandex.ru>
2023-08-05 16:41:07 +0500

    [production-project] add feature-sliced architecture

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,10 @@
 import {Routes, Route, Link} from 'react-router-dom';
-import {AboutPageAsync} from "./pages/AboutPage/AboutPageAsync";
-import {MainPageAsync} from "./pages/MainPage/MainPageAsync";
 import {Suspense} from "react";
 import "./styles/index.scss";
-import {useTheme} from "./theme/useTheme";
-import {classNames} from "./helpers/classNames/classNames";
+import {useTheme} from "app/providers/ThemeProvider";
+import {classNames} from "shared/lib/classNames/classNames";
+import {AboutPage} from "pages/AboutPage";
+import {MainPage} from "pages/MainPage";
 
 
 export const App = () => {
@@ -18,8 +18,8 @@ export const App = () => {
 
             <Suspense fallback={<div>Loading...</div>}>
                 <Routes>
-                    <Route path={'/about'} element={<AboutPageAsync/>}/>
-                    <Route path={'/'} element={<MainPageAsync/>}/>
+                    <Route path={'/about'} element={<AboutPage/>}/>
+                    <Route path={'/'} element={<MainPage/>}/>
                 </Routes>
             </Suspense>
         </div>
--- src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
+++ src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
@@ -1,4 +1,4 @@
-import {LOCAL_STORAGE_KEY, Theme, ThemeContext} from "./ThemeContext";
+import {LOCAL_STORAGE_KEY, Theme, ThemeContext} from "../lib/ThemeContext";
 import {FC, useMemo, useState} from "react";
 
 const defaultTheme = localStorage.getItem(LOCAL_STORAGE_KEY) as Theme || Theme.Light;
