c0246b86719c2595b17658b15fc9b11f41f206e5
Alexander <kolo-ky@yandex.ru>
2023-08-06 14:37:33 +0500

    [production-project] add config router

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,8 @@
-import {Routes, Route, Link} from 'react-router-dom';
-import {Suspense} from "react";
+import {Link} from 'react-router-dom';
 import "./styles/index.scss";
 import {useTheme} from "app/providers/ThemeProvider";
 import {classNames} from "shared/lib/classNames/classNames";
-import {AboutPage} from "pages/AboutPage";
-import {MainPage} from "pages/MainPage";
+import {AppRouter} from "app/providers/router";
 
 
 export const App = () => {
@@ -16,12 +14,7 @@ export const App = () => {
             <Link to={'/'}>Main</Link>
             <Link to={'/about'}>About</Link>
 
-            <Suspense fallback={<div>Loading...</div>}>
-                <Routes>
-                    <Route path={'/about'} element={<AboutPage/>}/>
-                    <Route path={'/'} element={<MainPage/>}/>
-                </Routes>
-            </Suspense>
+            <AppRouter/>
         </div>
     );
 };
