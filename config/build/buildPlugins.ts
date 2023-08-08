6839b100bf2b5ee5abdc689d9d3f4b6abab80fee
Alexander <kolo-ky@yandex.ru>
2023-08-09 00:23:22 +0500

    [production-project] add hot refresh plugin

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -12,6 +12,7 @@ export function buildPlugins ({paths}: IBuildOptions): webpack.WebpackPluginInst
         new MiniCssExtractPlugin({
             filename: 'css/[name].[contenthash:8].css',
             chunkFilename: 'css/[name].[contenthash:8].css'
-        })
+        }),
+        new webpack.HotModuleReplacementPlugin()
     ]
 }
