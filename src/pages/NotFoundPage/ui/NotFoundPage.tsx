84b9e6c009fe5002c6830a147d5c150f225b007c
Alexander <kolo-ky@yandex.ru>
2023-08-12 12:33:32 +0500

    [production-project] add not found page

---
+++ src/pages/NotFoundPage/ui/NotFoundPage.tsx
@@ -0,0 +1,13 @@
+import { FC } from 'react';
+
+interface INotFoundPageProps {
+    className?: string
+}
+
+export const NotFoundPage: FC<INotFoundPageProps> = () => {
+    return (
+        <div>
+            NotFoundPage
+        </div>
+    );
+};
