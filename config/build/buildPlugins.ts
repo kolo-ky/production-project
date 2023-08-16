254f23cb4efe91eb0e0e48565dd891638c97305a
Alexander <kolo-ky@yandex.ru>
2023-08-16 23:05:40 +0500

    [production-project] refactor buildPlugins config

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -4,8 +4,8 @@ import { IBuildOptions } from './types/config';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 
-export function buildPlugins({ paths }: IBuildOptions): webpack.WebpackPluginInstance[] {
-    return [
+export function buildPlugins({ paths, isDev }: IBuildOptions): webpack.WebpackPluginInstance[] {
+    const plugins = [
         new HTMLWebpackPlugin({
             template: paths.html
         }),
@@ -18,5 +18,14 @@ export function buildPlugins({ paths }: IBuildOptions): webpack.WebpackPluginIns
         new BundleAnalyzerPlugin({
             openAnalyzer: false
         })
-    ]
+    ];
+
+    if (isDev) {
+        plugins.push(new webpack.HotModuleReplacementPlugin());
+        plugins.push(new BundleAnalyzerPlugin({
+            openAnalyzer: false
+        }));
+    }
+
+    return plugins;
 }
